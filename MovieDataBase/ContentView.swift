c5af64789449be4fdfe9e70726aaae81402b3379
Gembali Sandesh Kumar <93411433+gembalisandesh@users.noreply.github.com>
2024-10-22 11:23:38 +0530

    Initial Commit

---
+++ MovieDataBase/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MovieDataBase
+//
+//  Created by gembali sandesh kumar on 22/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MovieDataBase/MovieDataBaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieDataBaseApp.swift
+//  MovieDataBase
+//
+//  Created by gembali sandesh kumar on 22/10/24.
+//
+
+import SwiftUI
+
+@main
+struct MovieDataBaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
